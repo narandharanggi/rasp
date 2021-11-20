644258b4487ac022ee44827bd1d1708268678379
Tanzilal Mustaqim <derekln14@gmail.com>
2021-11-20 22:46:58 +0700

    add splash screen and utils menu

---
+++ app/src/main/java/com/cun/rasp/view/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.cun.rasp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.cun.rasp.MainActivity;
+import com.cun.rasp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
---
+++ app/src/main/java/com/cun/rasp/view/UbahDataActivity.java
@@ -0,0 +1,16 @@
+package com.cun.rasp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cun.rasp.R;
+
+public class UbahDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_data);
+    }
+}
