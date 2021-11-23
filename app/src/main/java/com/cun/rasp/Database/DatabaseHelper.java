b0165505408038e2def5627fd04d9a82d27ec12f
Tanzilal Mustaqim <derekln14@gmail.com>
2021-11-23 14:15:11 +0700

    add model fetch all

--- app/src/main/java/com/cun/rasp/Database/DatabaseHelper.java
+++ app/src/main/java/com/cun/rasp/Database/DatabaseHelper.java
@@ -17,6 +17,9 @@ import com.cun.rasp.model.ProduksiSusu;
 import com.cun.rasp.model.Sapi;
 import com.cun.rasp.model.perBB;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Logcat tag
@@ -436,7 +439,231 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return td;
     }
 
-
     //get all data
+
+@SuppressLint("Range")
+public List<Sapi> getAllSapis() {
+    List<Sapi> Sapis = new ArrayList<Sapi>();
+    String selectQuery = "SELECT  * FROM " + Sapi.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            Sapi td = new Sapi();
+            td.setId(c.getInt(c.getColumnIndex(Sapi.COLUMN_ID)));
+            td.setProduksiSusu((c.getInt(c.getColumnIndex(Sapi.COLUMN_PRODUKSI_SUSU))));
+            td.setBobot((c.getInt(c.getColumnIndex(Sapi.COLUMN_BOBOT))));
+            td.setBk((c.getDouble(c.getColumnIndex(Sapi.COLUMN_BK))));
+
+            // adding to Sapi list
+            Sapis.add(td);
+        } while (c.moveToNext());
+    }
+
+    return Sapis;
+}
+
+@SuppressLint("Range")
+public List<ProduksiSusu> getAllProduksiSusus() {
+    List<ProduksiSusu> ProduksiSusus = new ArrayList<ProduksiSusu>();
+    String selectQuery = "SELECT  * FROM " + ProduksiSusu.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            ProduksiSusu td = new ProduksiSusu();
+            td.setId(c.getInt(c.getColumnIndex(ProduksiSusu.COLUMN_ID)));
+            td.setProduksiSusu((c.getInt(c.getColumnIndex(ProduksiSusu.COLUMN_PRODUKSI_SUSU))));
+
+            // adding to ProduksiSusu list
+            ProduksiSusus.add(td);
+        } while (c.moveToNext());
+    }
+
+    return ProduksiSusus;
+}
+
+@SuppressLint("Range")
+public List<perBB> getAllperBBs() {
+    List<perBB> perBBs = new ArrayList<perBB>();
+    String selectQuery = "SELECT  * FROM " + perBB.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            perBB td = new perBB();
+            td.setId(c.getInt(c.getColumnIndex(perBB.COLUMN_ID)));
+            td.setKeterangan((c.getString(c.getColumnIndex(perBB.COLUMN_KETERANGAN))));
+            td.setTdn(c.getDouble(c.getColumnIndex(perBB.COLUMN_TDN)));
+            td.setPk(c.getDouble(c.getColumnIndex(perBB.COLUMN_PK)));
+
+            // adding to perBB list
+            perBBs.add(td);
+        } while (c.moveToNext());
+    }
+
+    return perBBs;
+}
+
+@SuppressLint("Range")
+public List<LemakSusu> getAllLemakSusus() {
+    List<LemakSusu> LemakSusus = new ArrayList<LemakSusu>();
+    String selectQuery = "SELECT  * FROM " + LemakSusu.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            LemakSusu td = new LemakSusu();
+            td.setId(c.getInt(c.getColumnIndex(LemakSusu.COLUMN_ID)));
+            td.setPersenLemak((c.getDouble(c.getColumnIndex(LemakSusu.COLUMN_PERSEN_LEMAK))));
+            td.setTdn(c.getDouble(c.getColumnIndex(LemakSusu.COLUMN_TDN)));
+            td.setPk(c.getDouble(c.getColumnIndex(LemakSusu.COLUMN_PK)));
+            td.setCa(c.getDouble(c.getColumnIndex(LemakSusu.COLUMN_CA)));
+            td.setP(c.getDouble(c.getColumnIndex(LemakSusu.COLUMN_P)));
+
+            // adding to LemakSusu list
+            LemakSusus.add(td);
+        } while (c.moveToNext());
+    }
+
+    return LemakSusus;
+}
+    
+@SuppressLint("Range")
+public List<DetailSapi> getAllDetailSapis() {
+    List<DetailSapi> DetailSapis = new ArrayList<DetailSapi>();
+    String selectQuery = "SELECT  * FROM " + DetailSapi.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            DetailSapi td = new DetailSapi();
+            td.setId(c.getInt(c.getColumnIndex(DetailSapi.COLUMN_ID)));
+            td.setSapi((c.getInt(c.getColumnIndex(DetailSapi.COLUMN_SAPI))));
+            td.setLemakSusu(c.getInt(c.getColumnIndex(DetailSapi.COLUMN_LEMAK_SUSU)));
+            td.setperBB(c.getInt(c.getColumnIndex(DetailSapi.COLUMN_perBB)));
+
+            // adding to DetailSapi list
+            DetailSapis.add(td);
+        } while (c.moveToNext());
+    }
+
+    return DetailSapis;
+}
+
+
+@SuppressLint("Range")
+public List<DetailPakan> getAllDetailPakans() {
+    List<DetailPakan> DetailPakans = new ArrayList<DetailPakan>();
+    String selectQuery = "SELECT  * FROM " + DetailPakan.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            DetailPakan td = new DetailPakan();
+            td.setId(c.getInt(c.getColumnIndex(DetailPakan.COLUMN_ID)));
+            td.setSapi((c.getInt(c.getColumnIndex(DetailPakan.COLUMN_SAPI))));
+            td.setBahanPakan(c.getInt(c.getColumnIndex(DetailPakan.COLUMN_BAHAN_PAKAN)));
+
+            // adding to DetailPakan list
+            DetailPakans.add(td);
+        } while (c.moveToNext());
+    }
+
+    return DetailPakans;
+}
+
+@SuppressLint("Range")
+public List<BobotSapi> getAllBobotSapis() {
+    List<BobotSapi> BobotSapis = new ArrayList<BobotSapi>();
+    String selectQuery = "SELECT  * FROM " + BobotSapi.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            BobotSapi td = new BobotSapi();
+            td.setId(c.getInt(c.getColumnIndex(BobotSapi.COLUMN_ID)));
+            td.setBobot((c.getDouble(c.getColumnIndex(BobotSapi.COLUMN_BOBOT))));
+    
+            td.setCa(c.getDouble(c.getColumnIndex(BobotSapi.COLUMN_CA)));
+    
+            td.setP(c.getDouble(c.getColumnIndex(BobotSapi.COLUMN_P)));
+            td.setPk(c.getDouble(c.getColumnIndex(BobotSapi.COLUMN_PK)));
+            td.setTdn(c.getDouble(c.getColumnIndex(BobotSapi.COLUMN_TDN)));
+
+            // adding to BobotSapi list
+            BobotSapis.add(td);
+        } while (c.moveToNext());
+    }
+
+    return BobotSapis;
+}
+
+@SuppressLint("Range")
+public List<BahanPakan> getAllBahanPakans() {
+    List<BahanPakan> BahanPakans = new ArrayList<BahanPakan>();
+    String selectQuery = "SELECT  * FROM " + BahanPakan.TABLE_NAME;
+ 
+    Log.e(LOG, selectQuery);
+ 
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selectQuery, null);
+
+    // looping through all rows and adding to list
+    if (c.moveToFirst()) {
+        do {
+            BahanPakan td = new BahanPakan();
+            td.setId(c.getInt(c.getColumnIndex(BahanPakan.COLUMN_ID)));
+            td.setBk((c.getDouble(c.getColumnIndex(BahanPakan.COLUMN_BK))));
+            td.setNamaPakan(c.getString(c.getColumnIndex(BahanPakan.COLUMN_NAMA_PAKAN)));
+            td.setCa(c.getDouble(c.getColumnIndex(BahanPakan.COLUMN_CA)));
+            td.setHarga(c.getInt(c.getColumnIndex(BahanPakan.COLUMN_HARGA)));
+            td.setP(c.getDouble(c.getColumnIndex(BahanPakan.COLUMN_P)));
+            td.setPk(c.getDouble(c.getColumnIndex(BahanPakan.COLUMN_PK)));
+            td.setTdn(c.getDouble(c.getColumnIndex(BahanPakan.COLUMN_TDN)));
+            // adding to BahanPakan list
+            BahanPakans.add(td);
+        } while (c.moveToNext());
+    }
+
+    return BahanPakans;
+}
+
+
 
 }
